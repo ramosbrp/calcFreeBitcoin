5c3bda943f196e4597bddc3152f3d3fad6c4867d
bruno <bruno.rp45@gmail.com>
2021-05-24 22:21:08 -0300

    Correção do arquivo app.js: retirada do document.write e add document.getElement. Correção do arquivo inde.html: Modificação na lista

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const somarValor = (valor) => Math.round(valor * 1.4);
 
 const somarI = (i) => (i += 1);
 
-const somarApostas = (n1, n2) => n1 + n2;
+const somarApostas = (n1, n2) => n1 + n2 ;
 
 const calcGanhoReal = (n1, n2) => n1 - n2;
 /// --- ///
@@ -14,54 +14,53 @@ function myFunction() {
   //Declaração de variáveis
   let num = Number.parseInt(document.querySelector("#entrada").value);
   let contagem = 1;
-  let total = num;
-  let { ganha, ganhoReal } = multi(num);
+  var total = num;
+  let ganha = multi(num);
+	let ganhoReal = ganha
   let list = [];
   let i = 0;
 
-  const list1 = (lista, total) => (list = [...lista, total]);
 
-  list1(list, total);
+	list.push(total)
+	
+	let html = ''
 
-  //Ganhos e perda iniciais declaradas aqui
-  document.write("Aposta atual: ", num, "<br>");
-  document.write("Número de apostas: ", contagem, "<br>");
-  document.write("Total: ", total, "<br>");
-  document.write("Ganhar: ", ganha, "<br>");
-  document.write("Ganho real: ", ganhoReal, "<br>", "<br>");
+	html += '<p>'+'Aposta atual: '+num+'</p>'
+	html += '<p>'+'Número de apostas: '+contagem+'</p>'
+	html += '<p>'+'Total apostado: '+total+'</p>'
+	html += '<p>'+'Se ganhar: '+ganha+'</p>'
+	html += '<p>'+'Ganho real: '+ganhoReal+'</p>'
+	html += '<hr/>'
+	 document.getElementById('lista').innerHTML = html
 
   //Laço while para execução do programa
-  while (total <= 1700000) {
-    const apostado = (num = somarValor(num));
+  while (total <= 100) {
 
-    document.write("Aposta atual: ", apostado, "<br>");
-    document.write(
-      "Número de apostas: ",
-      (contagem = somarI(contagem)),
-      "<br>"
-    );
-    document.write("Total: ", (total = somarApostas(total, num)), "<br>");
-    list1(list, total);
+    const apostado = num = somarValor(num);
+	html += '<p>'+'Aposta atual: '+apostado+'</p>'
 
-    const ganhar = multi(apostado);
+	const contagem2 = somarI(contagem)
+	html += '<p>'+'Número de apostas: '+contagem2+'</p>'
 
-    document.write("Ganhar: ", ganhar, "<br>");
-    document.write(
-      "Ganho real: ",
-      calcGanhoReal(ganhar, list[i]),
-      "<br>",
-      "<br>"
-    );
-    i++;
-  }
+	const total2 = somarApostas(total, apostado)
+	html += '<p>'+'Total apostado: '+total2+'</p>'
+	list.push(total)
+
+	const ganhar = multi(apostado)
+	html += '<p>'+'Se ganhar: '+apostado+'</p>'
+	html += '<p>'+'Ganho real: '+calcGanhoReal(ganhar, list[i])+'</p>'
+	html += '<hr/>'
+	document.getElementById('lista').innerHTML = html
+	i++;
+  } 
 
   /*function diminuirValor(n2){
 	return Math.round(n2/1.4)
 }*/
 
   //Alteração do corpo após clicar
-  var corpo = document.body;
+  //var corpo = document.body;
 
-  corpo.style.background = "rgb(0, 122, 92)";
-  corpo.style.color = "#ffb13d";
+ // corpo.style.background = "rgb(0, 122, 92)";
+  //corpo.style.color = "#ffb13d";
 }
